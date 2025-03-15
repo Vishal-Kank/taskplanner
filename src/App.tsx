c9aa94a6dacc318889c45fd0e4ca1a8147672c5f
Vishal Kank <Kankvishalkank@gmail.com>
2025-03-16 02:37:26 +0530

    main | insatll router and setup example pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router";
+import Dashboard from "./pages/dashboard";
+import Header from "./pages/header";
+import Footer from "./pages/footer";
+import TaskDetails from "./pages/task_details";
+
 function App() {
   return (
     <>
       <h1>TASK PLANNER</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        {/*http://localhost:3000/task_details/123*/}
+        <Route path="/task_details/:taskID" element={<TaskDetails />} /> 
+      </Routes>
+      <Footer />
     </>
   );
 }
