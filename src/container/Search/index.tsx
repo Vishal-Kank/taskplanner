0c184ddca235d42683c96ca9a2e6da664fa4e749
Vishal Kank <Kankvishalkank@gmail.com>
2025-06-07 01:20:54 +0530

    Create layout for Search

--- src/container/Search/index.tsx
+++ src/container/Search/index.tsx
@@ -1,20 +1,61 @@
-import { Input } from "@mui/material";
+import { InputBase, styled } from "@mui/material";
+
+type InputStyledProp = {
+  variant?: "outlined" | "filled" | "standard";
+};
+
+const InputStyled = styled(InputBase, {
+  shouldForwardProp: (prop) => prop !== "variant",
+})<InputStyledProp>(({ variant }) => ({
+  border: "none",
+  position: "relative",
+  "&::before": {
+    display: "none", // remove default underline
+  },
+  "&::after": {
+    content: '""',
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    height: "2px",
+    width: "100%",
+    backgroundColor: "#DDD",
+    transform: "scaleX(0)",
+    transformOrigin: "left",
+    transition: "transform 0.3s ease-in-out",
+    pointerEvents: "none",
+  },
+  "&:focus-within::after": {
+    transform: "scaleX(1)",
+  },
+
+  ...(variant === "outlined" && {
+    border: "2px solid #DDD",
+    padding: "0.2rem 0.8rem",
+    borderRadius: "8px",
+  }),
+  ...(variant === "filled" && {
+    background: "#DDD",
+    padding: "0.2rem 0.8rem",
+    borderRadius: "8px",
+    color: "#000",
+  }),
+  ...(variant === "standard" && {
+    borderBottom: "2px solid #DDD",
+    padding: "0.2rem 0",
+  }),
+}));
 
 const Search = () => {
   return (
-    <Input
-      fullWidth
-      disableUnderline
-      sx={{
-        border: "1px solid #DDD",
-        borderRadius: "4px",
-        padding: "8px 12px",
-        fontSize: "0.85rem",
-      }}
-      inputProps={{ style: { padding: 0 } }}
-      placeholder="Search Task"
-    />
+    <InputStyled fullWidth variant={"outlined"} placeholder="Search Task" />
   );
 };
 
 export default Search;
+
+// Pendding...........
+// Apply throttling
+// Display results (need to design and think)
+// clean the code
+// apply state
